ff10ef56d64e9c9bd53c5f37c7d227f1806f727d
Maycon Mendes <maycon.mendes@senior.com.br>
2022-07-15 11:59:20 -0300

    [FEAT] - Estiliza fotos, cria grid-template e estiliza botão para carregar mais imagens.

--- app.js
+++ app.js
@@ -40,8 +40,12 @@ function generatePictures(data) {
     data.photos.forEach((photo) => {
         const galleryImg = document.createElement('div');
         galleryImg.classList.add('gallery-img');
-        galleryImg.innerHTML = `<img src=${photo.src.large}></img>
+        galleryImg.innerHTML = `
+        <div class="gallery-info">
         <p>${photo.photographer}</p>
+        <a href=${photo.src.original}>Download</a>
+        </div>
+        <img src=${photo.src.large}></img>
         `;
         gallery.appendChild(galleryImg);
     })
@@ -53,8 +57,14 @@ async function curatedPhotos() {
 }
 
 async function searchPhotos(query) {
+    clear();
     const data = await fetchApi(`https://api.pexels.com/v1/search?query=${query}`)
     generatePictures(data);
+}
+
+function clear() {
+    gallery.innerHTML = '';
+    searchInput.value = '';
 }
 
 
