9d0c31ddab60a9b3b267562ea52fd09302d950fc
Maycon Mendes <maycon.mendes@senior.com.br>
2022-07-15 13:00:53 -0300

    [FEAT] - Cria funcionalidade para o botão more, carregando mais imagens.

--- app.js
+++ app.js
@@ -7,16 +7,21 @@ const authorization = '563492ad6f91700001000001201f2d708b5e4e34a3217b218ec6e1aa'
 const gallery = document.querySelector('.gallery');
 const searchInput = document.querySelector('.search-input');
 const form = document.querySelector('.search-form');
+const more = document.querySelector('.more');
 
 let searchValue;
-
+let fetchLink;
+let page = 1;
+let currentSearch;
 
 // Events
 searchInput.addEventListener('input', updateInput);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    currentSearch = searchValue;
     searchPhotos(searchValue);
 })
+more.addEventListener('click', loadMore);
 
 // Functions
 function updateInput(e) {
@@ -52,19 +57,33 @@ function generatePictures(data) {
 }
 
 async function curatedPhotos() {
-    const data = await fetchApi('https://api.pexels.com/v1/curated');
+    fetchLink = 'https://api.pexels.com/v1/curated?per_page=15&page=1';
+    const data = await fetchApi(fetchLink);
     generatePictures(data);
 }
 
 async function searchPhotos(query) {
     clear();
-    const data = await fetchApi(`https://api.pexels.com/v1/search?query=${query}`)
+    fetchLink = `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`
+    const data = await fetchApi(fetchLink);
     generatePictures(data);
 }
 
 function clear() {
     gallery.innerHTML = '';
     searchInput.value = '';
+}
+
+async function loadMore() {
+    page++;
+    if(currentSearch) {
+        fetchLink = `https://api.pexels.com/v1/search?query=${currentSearch}+query&per_page=15&page=${page}`;
+    } else {
+        fetchLink = `https://api.pexels.com/v1/curated?per_page=15&page=${page}`;
+    }
+
+    const data = await fetchApi(fetchLink);
+    generatePictures(data);
 }
 
 
